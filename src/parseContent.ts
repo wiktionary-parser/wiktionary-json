12e9d3bdd94cf1c7c53b78f3d6097eb2a8690ff0
Wagner Leonardi <leonardiwagner@gmail.com>
2021-08-07 17:35:44 +0200

    test updates

--- src/parseContent.ts
+++ src/parseContent.ts
@@ -9,7 +9,7 @@ const HEADER_ELEMENTS = ['h1','h2', 'h3', 'h4', 'h5']
 const isHeaderElement = (e: cheerio.TagElement) => HEADER_ELEMENTS.includes(e.name)
 const getHeadingLevel = (headingTag: string) => parseInt(headingTag.substr(1, 1))
 
-const parseContent2 = (elementsToParse: cheerio.TagElement[], parsedItems: Item[], lastParsedItem: Item): Item[] => {
+const parse = (elementsToParse: cheerio.TagElement[], parsedItems: Item[], lastParsedItem: Item): Item[] => {
     const [e, ...restOfElements] = elementsToParse; 
 
     if(!e) return parsedItems
@@ -30,7 +30,7 @@ const parseContent2 = (elementsToParse: cheerio.TagElement[], parsedItems: Item[
 
             lastParsedItem.children.push(newHeaderItem)
 
-            return parseContent2(restOfElements, parsedItems, newHeaderItem)
+            return parse(restOfElements, parsedItems, newHeaderItem)
         }
 
         if(levelDifference == 0) {
@@ -51,7 +51,7 @@ const parseContent2 = (elementsToParse: cheerio.TagElement[], parsedItems: Item[
 
             parentItem.children.push(newHeaderItem)
 
-            return parseContent2(restOfElements, parsedItems, newHeaderItem)
+            return parse(restOfElements, parsedItems, newHeaderItem)
         }
 
         if(levelDifference < 0) {
@@ -71,85 +71,16 @@ const parseContent2 = (elementsToParse: cheerio.TagElement[], parsedItems: Item[
 
             parentItem.children.push(newHeaderItem)
 
-            return parseContent2(restOfElements, parsedItems, newHeaderItem)
+            return parse(restOfElements, parsedItems, newHeaderItem)
         }
 
     }
 
-    lastParsedItem.elements.push($(e).html())
+    //lastParsedItem.elements.push($(e).html())
 
-    return parseContent2(restOfElements, parsedItems, lastParsedItem)
+    return parse(restOfElements, parsedItems, lastParsedItem)
 }
 
-const parseInnerContent = (elementsToParse: cheerio.TagElement[], parsedItems: Item[], lastParsedItem: Item): Item[] => {
-    const [e, ...restOfElements] = elementsToParse; 
-
-    if(!e) return parsedItems
-
-    if(isHeaderElement(e)) {
-        const levelDifference = getHeadingLevel(e.name) - lastParsedItem._headingLevel
-
-        if(levelDifference > 0) {
-            const newHeaderItem: Item = {
-                _id: lastParsedItem._id + 1,
-                _parentId: lastParsedItem._id,
-                _headingLevel: getHeadingLevel(e.name),
-                title: $(e).find('span:first-child').text(),
-                children: [],
-                elements: []
-            }
-
-            lastParsedItem.children.push(newHeaderItem)
-
-            return parseInnerContent(restOfElements, parsedItems, newHeaderItem)
-        }
-        
-        if(levelDifference == 0) {
-            const parentItem = findItemsById(parsedItems, lastParsedItem._parentId)
-
-            if(!parentItem) {
-                throw new Error('note faunde')
-            }
-
-            const newHeaderItem: Item = {
-                _id: lastParsedItem._id + 1,
-                _parentId: parentItem._id,
-                _headingLevel: getHeadingLevel(e.name),
-                title: $(e).find('span:first-child').text(),
-                children: [],
-                elements: []
-            }
-
-            parentItem.children.push(newHeaderItem)
-
-            return parseInnerContent(restOfElements, parsedItems, newHeaderItem)
-        }
-
-
-        if(levelDifference < 0) {
-            const positiveDiference = levelDifference * -1 + 1
-
-            const parentItem = navigateToParent(positiveDiference, lastParsedItem, parsedItems)
-
-            const newHeaderItem: Item = {
-                _id: lastParsedItem._id + 1,
-                _parentId: parentItem._id,
-                _headingLevel: getHeadingLevel(e.name),
-                title: $(e).find('span:first-child').text(),
-                children: [],
-                elements: []
-            }
-
-            parentItem.children.push(newHeaderItem)
-
-            return parseInnerContent(restOfElements, parsedItems, newHeaderItem)
-        }
-    }
-
-    lastParsedItem.elements.push(e)
-    
-    return parseInnerContent(restOfElements, parsedItems, lastParsedItem)
-}
 
 const parseContent = (content: Buffer) : Item[] => {
     $ = cheerio.load(content);
@@ -164,7 +95,7 @@ const parseContent = (content: Buffer) : Item[] => {
         elements: []
     }
 
-    return parseContent2(articleElements, [item], item)
+    return parse(articleElements, [item], item)
 }
 
 export default parseContent;
--- test/parseContent.test.ts
+++ test/parseContent.test.ts
@@ -2,15 +2,31 @@ import * as fs from 'fs';
 import parseContent from '../src/parseContent'
 
 fdescribe('parse content', () => {
-
-    const fileBuffer = fs.readFileSync('./test/document.html');
-    const result = parseContent(fileBuffer)
-    console.log("result", result)
-    console.log()
-
-    fs.writeFileSync('./ople.json', JSON.stringify(result, undefined, 2))
-
     it('should parse', () => {
-        expect('c').toBe('c')
+        const fileBuffer = fs.readFileSync('./test/document.html');
+        const result = parseContent(fileBuffer)
+
+        //console.log("result", JSON.stringify(result, undefined, 2))
+
+        const headItem = result[0]
+
+        
+        // only the head element
+        expect(result.length).toBe(1)
+
+        expect(headItem.title).toBe('pool')
+        expect(headItem.children.length).toBe(9)
+
+        const firstChild = headItem.children[0]
+        expect(firstChild.title).toBe('English')
+        expect(firstChild.children.length).toBe(4)
+
+        const firstChildChild = firstChild.children[0]
+        expect(firstChildChild.title).toBe('Pronunciation')
+        expect(firstChildChild.children.length).toBe(0)
+
+        const secondChildChild = firstChild.children[1]
+        expect(secondChildChild.title).toBe('Etymology 1')
+        expect(secondChildChild.children.length).toBe(2)
     })
 })
